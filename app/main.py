398babb2dcbadedefc7e3e594b19cffbc883e4ab
Yuki <k701user@gmail.com>
2018-04-12 11:50:14 +0900

    Update main.py
    Add API option "sammarize"

--- app/main.py
+++ app/main.py
@@ -32,5 +32,19 @@ def newsreader_debug():
       return 'not found : %s' % query, 400
     return result, 200
 
+
+@app.route('/sammarize', methods=['GET'])
+def newsreader():
+    """Given an query, return that news."""
+    query = request.args.get('query')
+    rowcount = request.args.get('row')
+    if query is None:
+      return 'No provided.', 400
+    result = SL.sammarize(query, rowcount)
+    if result is None:
+      return 'not found : %s' % query, 400
+    return result, 200
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
